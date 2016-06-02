a204fe14ea6017390a4a0eb02f28c4e72db60622
Jinu <pcjinu@ymail.com>
2016-06-02 20:03:19 +0530

    Testing debug mode

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'deWelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('home', 'deHomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
