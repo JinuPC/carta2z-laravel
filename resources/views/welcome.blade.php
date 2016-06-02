420adb7950efd4f77073a9093fa17777a461967b
Jinu <pcjinu@ymail.com>
2016-06-02 20:55:45 +0530

    added everything checking status

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,24 @@
+
 <?php
-	global $admin_url, $login_url, $register_url;
+    global $admin_url, $login_url, $register_url;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>UniMarket - Your Integrated Maket Place</title>
-	<base href="{{ url('/') }}/" />
-	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-	<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700') }}">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <meta charset="UTF-8">
+    <title>UniMarket - Your Integrated Maket Place</title>
+    <base href="{{ url('/') }}/" />
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700') }}">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/ionicons.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-	<link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}" type="text/css">
-	<link rel="stylesheet" href="{{ asset('css/animate.min.css') }}" type="text/css">
-	<link rel="stylesheet" href="{{ asset('css/creative.css') }}" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/ionicons.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/creative.css') }}" type="text/css">
     <style>
         .unimarket {
             size : larger;
@@ -25,12 +26,12 @@
         
     </style>
 
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-	<![endif]-->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body id="page-top">
@@ -51,8 +52,8 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                	
-					
+                    
+                    
                     <li>
                         <a class="page-scroll " href="#about">About</a>
                     </li>
@@ -66,14 +67,14 @@
                         <a class="page-scroll" href="#contact">Contact</a>
                     </li>
                     <li>
-						<a href="/work">How it works</a>
-					</li>
-                    <li>
-                    	<a href="{{ $register_url }}">SignUp</a>
+                        <a href="/work">How it works</a>
                     </li>
-					<li>
-						<a href="{{ $login_url }}">SignIn</a>
-					</li>
+                    <li>
+                        <a href="{{ $register_url }}">SignUp</a>
+                    </li>
+                    <li>
+                        <a href="{{ $login_url }}">SignIn</a>
+                    </li>
 
                 </ul>
             </div>
@@ -276,9 +277,9 @@
     </section>
 
 
-	<script type="text/javascript" src="{{ asset('js/jquery-1.11.3.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-	<script src="js/jquery.js"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery-1.11.3.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
@@ -290,7 +291,7 @@
  
     <!-- Custom Theme JavaScript -->
     <script src="{{ asset('js/creative.js') }}"></script>
-	@include('adminbar')
+    @include('adminbar')
 
 </body>
 </html>
